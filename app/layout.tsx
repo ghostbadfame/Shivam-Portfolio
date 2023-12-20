ec8553a26812467003ffbd227f5261b75f6f61ee
Shivam Kapoor <kapoorshivam77@gmail.com>
2023-12-20 14:25:38 +0530

    Logo update-1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
+import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "SHIVAM",
-  description: "WEB DEVELOPMENT AND DESIGN PORTFOLIO OF SHIVAM KAPOOR",
-  themeColor: "white",
-  creator: "Shivam Kapoor",
-};
+// export const metadata = {
+//   title: "Shivam",
+//   description: "WEB DEVELOPMENT AND DESIGN PORTFOLIO OF SHIVAM KAPOOR",
+//   themeColor: "white",
+//   creator: "Shivam Kapoor",
+//   icon: "/icon.jpg"
+  
+// };
 
 export default function RootLayout({
   children,
@@ -16,6 +19,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>shivam</title>
+        <link rel="icon" href={"/icon2.ico"} />
+        <meta name="description" content={"WEB DEVELOPMENT AND DESIGN PORTFOLIO OF SHIVAM KAPOOR"} />
+        <meta name="theme-color" content={"white"} />
+        <meta name="creator" content={"Shivam Kapoor"} />
+      </head>
       <body
         className={
           inter.className +
